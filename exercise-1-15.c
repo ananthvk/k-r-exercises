738591bebe6328d3436822666d757c70e8d77d7e
shankar <123456789987654321sr@gmail.com>
2022-06-29 00:11:52 +0530

    finished exercise 1-16

---
+++ exercise-1-15.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+float celsiusToFarenheit(float c){
+    return (9.0/5.0)*c + 32.0;
+}
+int main(){
+    for(int i = 0; i <= 100; i += 1){
+        printf("%-7d%-7.2f\n", i, celsiusToFarenheit(i));
+    }
+}
---
+++ exercise-1-16.c
@@ -0,0 +1,111 @@
+#define MAX_LINE_LENGTH 501
+
+#include<stdio.h>
+#include<assert.h>
+#include<stddef.h>
+int kr_getline(char buffer[], int bufferSize);
+void kr_strncpy(char src[], char dest[], int destSize);
+
+int main(){
+    char buffer[MAX_LINE_LENGTH], buffer2[MAX_LINE_LENGTH];
+    char longestLine[MAX_LINE_LENGTH];
+    int bufferLength, maxLength = 0, lineLength = 0;
+
+
+    while((bufferLength = kr_getline(buffer, MAX_LINE_LENGTH))){
+        // The last character of the buffer (excepting the null character), is
+        // not a newline, which means that the buffer is partial.
+        if((bufferLength == (MAX_LINE_LENGTH-1))
+            && (buffer[MAX_LINE_LENGTH - 2] != '\n')) {
+            if(lineLength == 0){
+                // This is the first section of the line.
+                kr_strncpy(buffer, buffer2, MAX_LINE_LENGTH);
+            }
+            lineLength += bufferLength;
+        }
+        // The buffer is partially filed and has a newline or it is completely
+        // filled with a newline at the end.
+        else{
+            if(lineLength > 0){
+                // This means that before this line, there are some incomplete 
+                // lines. So do  not overwrite the buffer.
+                lineLength += bufferLength;
+                if(lineLength > maxLength){
+                    maxLength = lineLength;
+                    kr_strncpy(buffer2, longestLine, MAX_LINE_LENGTH);
+                }
+                lineLength = 0;
+            }
+            else{
+                // There are no other incomplete lines before this line.
+                if(bufferLength > maxLength){
+                    maxLength = bufferLength;
+                    kr_strncpy(buffer, longestLine, MAX_LINE_LENGTH);
+                }
+            }
+        }
+    }
+    printf("Longest line:%4d characters\n", maxLength);
+    printf("%s\n", longestLine);
+}
+
+void kr_strncpy(char src[], char dest[], int destSize){
+    // Copies characters from source array to destination array.
+    // Also null terminates the destination array. 
+    
+    int i = 0;
+    while(1){
+        assert(i <= (destSize-1));
+        dest[i] = src[i];
+        if(src[i] == '\0')
+            return;
+
+        i++;
+        if(i == (destSize-1)){
+            // No more room to copy another character, destSize-2 location is 
+            // already filled, set character at index destSize-1 to null
+            // character.
+            dest[i] = '\0';
+            return;
+        }
+    }
+}
+
+int kr_getline(char buffer[], int bufferSize){
+    int ch, bufferIndex = 0, lineLength = 0;
+    while(1){
+        ch = getchar();
+
+        if(ch == EOF){
+            // The code to check for End of file is placed here, so that the 
+            // program does not add the EOF to the buffer, and also does not
+            // inrease the line length.
+            assert(bufferIndex <= (bufferSize-1));
+            buffer[bufferIndex] = '\0';
+            return lineLength;
+        }
+        assert(bufferIndex <= (bufferSize-1));
+
+        buffer[bufferIndex] = ch;
+        lineLength++;
+        bufferIndex++;
+
+        if(ch == '\n'){
+            // The below code guarantees that there is one vacant position at
+            // the end of the buffer, So it is not necessary to check 
+            // overflow condition here.
+            assert(bufferIndex <= (bufferSize-1));
+            buffer[bufferIndex] = '\0';
+            return lineLength;
+        }
+
+        if(bufferIndex == (bufferSize-1)){
+            // Here, bufferIndex is the index for the next character to be read.
+            // So if there is only one more location which is vacant in the buffer,
+            // set that to null character and return the string.
+            assert(bufferIndex <= (bufferSize-1));
+            buffer[bufferIndex] = '\0';
+            return lineLength;
+        }
+    }
+}

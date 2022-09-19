809aa2ee1895365867f7d7336cd6c177c0e9a000
shankar <123456789987654321sr@gmail.com>
2022-09-19 14:25:39 +0530

    Partially completed exercise-6-6

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# Programs which were not done by me
+
+0. entab and detab program from exercise 1
+1. Complete exercise-5-6, i.e implement other functions using pointers - I have only implemented getline using pointers.
+2. Exercise-5-7 - This program is about sorting strings using quick sort, which I have not understood - Revisit this problem after learning quicksort.
+3. Exercise-5-11, Exercise-5-12 - Not done as I have not done the entab and detab from exercise 1
+4. Exercise 5-17. Add a field-searching capability  - Not done as i have to learn how to sort using two columns
+5. Exercise 5-18 and others, based on dcl, a.k.a c declarations, do this after learning about recursive descent parser 
+
+Exercise 6-6
+===============
+What I learnt?
+1. There can be weird bugs such as in the process token function, wherein a
+space or newline character was printed twice
+2. The fix is to use if-else if-else ladder instead of plain ifs
+
+## TODO:
+This program is not fully complete and there are a few more features which are necessary.
+1. Does not handle quoted strings/chars in define.
+2. Does not ignore #define and replacement in comments/strings
+3. This code can be organized in a much better way
--- exercise-6-6.c
+++ exercise-6-6.c
@@ -49,39 +49,241 @@ void *safe_realloc(void *mem, size_t size);
 char *get_a_line(size_t *size);
 void exit_handler(const char *message, int exit_code);
 char *tokenize(const char *line, size_t *size);
-void exit_handler(const char *message, int exit_code)
+#define TABLE_SIZE 5  // 100003  // Nearest prime around 100k
+
+struct h_list {
+    struct h_list *next;
+    struct h_list *prev;
+    char *key;
+    char *value;
+};
+
+typedef struct h_list h_list;
+
+h_list *hash_table[TABLE_SIZE] = {NULL};
+
+h_list *h_list_create(const char *key, const char *value);
+h_list *h_list_append(h_list *parent, const char *key, const char *value);
+// Frees a single node
+void h_list_free_single(h_list *hl);
+// Frees the entire list starting from root
+void h_list_free(h_list *root);
+
+void table_store(const char *key, const char *value);
+// Look up the key in the table
+h_list *table_lookup(const char *key);
+// Really inefficient, TODO: Create a slab/arena allocator
+void table_free(void);
+// Removes the entry in the table (if present). Returns 1 if removal is
+// successful
+int table_remove(const char *key);
+unsigned int hash(const char *s);
+
+
+char *duplicatestr(const char *s)
+{
+    // +1 for the \0 character
+    char *buffer = safe_malloc(sizeof(char) * (strlen(s) + 1));
+    strcpy(buffer, s);
+    return buffer;
+}
+
+void *safe_malloc(size_t n)
+{
+    void *ptr = malloc(n);
+    if (!ptr) {
+        printf(
+            "Memory allocation failed! Check if your PC ran out of memory\n");
+        exit(2);
+    }
+    return ptr;
+}
+
+void *safe_realloc(void *memory, size_t new_size)
+{
+    void *ptr = realloc(memory, new_size);
+    if (!ptr) {
+        printf(
+            "Memory reallocation failed! Check if your PC ran out of memory\n");
+        free(memory);
+        exit(2);
+    }
+    return ptr;
+}
+
+h_list *h_list_create(const char *key, const char *value)
+{
+    h_list *new_node = safe_malloc(sizeof(h_list));
+    new_node->key = duplicatestr(key);
+    new_node->value = duplicatestr(value);
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    return new_node;
+}
+h_list *h_list_append(h_list *parent, const char *key, const char *value)
+{
+    // This is a modified version of append to list
+    // This function creates a new node if the key does not appear in the list
+    // otherwise it updates that node
+    if (parent) {
+        // Go to the last node
+        while (parent->next) {
+            if (strcmp(key, parent->key) == 0) {
+                parent->value = safe_realloc(parent->value, strlen(value) + 1);
+                strcpy(parent->value, value);
+                return parent;
+            }
+            parent = parent->next;
+        }
+
+        // To test if the first node has the same key as the given key
+        if (strcmp(key, parent->key) == 0) {
+            parent->value = safe_realloc(parent->value, strlen(value) + 1);
+            strcpy(parent->value, value);
+            return parent;
+        }
+        h_list *created_node = h_list_create(key, value);
+        parent->next = created_node;
+        created_node->prev = parent;
+        return created_node;
+    }
+    else {
+        return h_list_create(key, value);
+    }
+}
+void h_list_free_single(h_list *hl)
+{
+    if (hl) {
+        free(hl->key);
+        free(hl->value);
+        hl->key = NULL;
+        hl->value = NULL;
+        free(hl);
+    }
+}
+void h_list_free(h_list *root)
+{
+    h_list *tmp;
+    while (root) {
+        tmp = root;
+        root = root->next;
+        h_list_free_single(tmp);
+    }
+}
+
+void table_store(const char *key, const char *value)
+{
+    unsigned int hash_val = hash(key);
+    h_list *ins_pos = hash_table[hash_val];
+    if (!ins_pos) {
+        hash_table[hash_val] = h_list_create(key, value);
+    }
+    else {
+        h_list_append(hash_table[hash_val], key, value);
+    }
+}
+
+
+void table_store_fixed(const char *key, size_t m, const char *value, size_t n)
+{
+    // Stores the first m characters of key and first n characters of value
+    char *ky = duplicatestr(key); 
+    char *val = duplicatestr(value); 
+
+    if(m <= strlen(key))
+        ky[m] = '\0';
+    if(n <= strlen(value))
+        val[n] = '\0';
+
+    unsigned int hash_val = hash(ky);
+    h_list *ins_pos = hash_table[hash_val];
+    if (!ins_pos) {
+        hash_table[hash_val] = h_list_create(ky, val);
+    }
+    else {
+        h_list_append(hash_table[hash_val], ky, val);
+    }
+    free(ky);
+    free(val);
+}
+
+h_list *table_lookup(const char *key)
+{
+    if (key == NULL) return NULL;
+    unsigned int hash_val = hash(key);
+    h_list *node = hash_table[hash_val];
+    while (node) {
+        if (strcmp(key, node->key) == 0) return node;
+        node = node->next;
+    }
+    return NULL;
+}
+
+unsigned int hash(const char *s)
+{
+    // The djb2 hash function
+    // http://www.cse.yorku.ca/~oz/hash.html
+    unsigned int hash_val = 5381;
+    while (*s) {
+        hash_val = hash_val * 33 + (unsigned)*s;
+        s++;
+    }
+    return hash_val % TABLE_SIZE;
+}
+
+void table_free(void)
+{
+    // Really inefficient :/ Use a slab allocator
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (hash_table[i]) h_list_free(hash_table[i]);
+    }
+}
+
+void table_find(const char *key)
+{
+    h_list *node = table_lookup(key);
+    if (!node)
+        printf("%-12s : %s\n", key, "Key Error: Such a key does not exist");
+    else
+        printf("%-12s : %s\n", key, node->value);
+}
+
+int table_remove(const char *key)
+{
+    unsigned int calc_hash = hash(key);
+    h_list *node = table_lookup(key);
+    if (!node) return 0;
+    // Unlink the node
+    h_list *next_node = node->next;
+    h_list *prev_node = node->prev;
+
+    if (next_node == NULL && prev_node == NULL) {
+        // There is a single node in the list
+        hash_table[calc_hash] = NULL;
+    }
+
+    if (prev_node != NULL) {
+        prev_node->next = next_node;
+    }
+    if (next_node != NULL) {
+        if (prev_node == NULL) {
+            // This is the first node of the list
+            next_node->prev = NULL;
+            hash_table[calc_hash] = next_node;
+        }
+        else {
+            next_node->prev = prev_node;
+        }
+    }
+    h_list_free_single(node);
+    return 1;
+}
+ void exit_handler(const char *message, int exit_code)
 {
     printf("%s", message);
     exit(exit_code);
 }
 
-void *safe_malloc(size_t size)
-{
-    // Safe malloc function, exits the program incase of failure instead of
-    // returning NULL
-    void *ptr = malloc(size);
-    if (!ptr) {
-        exit_handler(
-            "Error: Memory allocation failed. Ensure that your computer has "
-            "enough memory\n",
-            1);
-    }
-    return ptr;
-}
-void *safe_realloc(void *mem, size_t size)
-{
-    // Safe realloc function, exits the program incase of failure instead of
-    // returning NULL
-    void *ptr = realloc(mem, size);
-    if (!ptr) {
-        free(mem);
-        exit_handler(
-            "Error: Memory reallocation failed. Ensure that your computer has "
-            "enough memory\n",
-            1);
-    }
-    return ptr;
-}
 
 char *get_a_line(size_t *size)
 {
@@ -99,7 +301,7 @@ char *get_a_line(size_t *size)
         if (ch == EOF) {
             break;
         }
-        buffer[buffer_size++] = ch;
+        buffer[buffer_size++] = (char)ch;
         if (ch == '\n') break;
 
         // Double allocated_buffer_size if the next character to be read will
@@ -128,7 +330,7 @@ char *tokenize(const char *line, size_t *size)
     }
     if (unget_required) {
         unget_required = 0;
-        *size = 1;
+        if(size) *size = 1;
         return lastchar;
     }
     char *beg = internal_line;
@@ -149,7 +351,7 @@ char *tokenize(const char *line, size_t *size)
 
 void print_tok(char *tok, size_t tok_size)
 {
-    printf("%.*s", tok_size, tok);
+    printf("%.*s", (int)tok_size, tok);
     // for(int i = 0; i < tok_size; i++) putchar(*(tok+i));
 }
 
@@ -177,6 +379,22 @@ void process_token(char *token, size_t token_size)
     // There can be any amount of space between define and NAME, NAME and VALUE
     // and VALUE and the newline
     // There should not be any spaces between # and define
+    char *token2 = NULL;
+    size_t token2_size = 0;
+
+    char *tok = duplicatestr(token);
+    if(token_size <= strlen(token))
+        tok[token_size] = '\0';
+    //printf("<%s>\n", tok);
+    h_list *lookup = table_lookup(tok);
+    free(tok);
+
+    if(lookup)
+    {
+        //printf("FOUND\n");
+        printf("%s",lookup->value);
+        return;
+    }
 
     if(token_size == 1)
     {
@@ -187,20 +405,26 @@ void process_token(char *token, size_t token_size)
             if(strncmp(token, "define", 6) == 0)
             {
                 // Skip whitespace
-                tokenize(NULL, &token_size);
+                tokenize(NULL, NULL);
 
                 // Read name
                 token = tokenize(NULL, &token_size);
-                printf("NAME:");
-                print_tok(token, token_size);
+                //print_tok(token, token_size);
 
                 // Skip whitespace
-                tokenize(NULL, &token_size);
+                tokenize(NULL, NULL);
 
                 // Read value
-                token = tokenize(NULL, &token_size);
-                printf(" VALUE:");
+                token2 = tokenize(NULL, &token2_size);
+                //printf(" VALUE:");
+                // print_tok(token, token_size);
+                table_store_fixed(token, token_size, token2, token2_size);
+
+                printf("#define");
+                printf(" ");
                 print_tok(token, token_size);
+                printf(" ");
+                print_tok(token2, token2_size);
             }
             else
             {
@@ -251,8 +475,15 @@ int main()
             break;
         }
     }
+    table_free();
 }
 // What I learnt?
 // 1. There can be weird bugs such as in the process token function, wherein a
 // space or newline character was printed twice
 // 2. The fix is to use if-else if-else ladder instead of plain ifs
+
+// TODO:
+// This program is not fully complete and there are a few more features which are necessary.
+// 1. Does not handle quoted strings/chars in define.
+// 2. Does not ignore #define and replacement in comments/strings
+// 3. This code can be organized in a much better way
